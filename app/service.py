33cd53bd92c210e4a9a6bb738b23183c8cd0e1d3
zeus <dhiogo.correa@itera.com.br>
2020-06-25 13:17:57 -0300

    API conn done

--- app/service.py
+++ app/service.py
@@ -1,21 +1,44 @@
 import requests
 import json
 
-API_URL = 'https://fiapdiagnosisapi.azurewebsites.net/Diagnostics'
+API_URL = 'https://fiapdiagnosisapi.azurewebsites.net/Diagnosis'
 
+
+def get_diagnostics_and_specializations(data):
+    diags = []
+    specialisations = []
+    for d in data:
+        if d['issue']['profNameTranslated']  not in diags:
+            diags.append(d['issue']['profNameTranslated'])
+        
+        for s in d['specialisation']:
+            if s['name'] not in specialisations:
+                specialisations.append(s['name'])
+    
+    return diags, specialisations
 
 def message(data):
     symps = data.selectedSymptoms.split(',')
 
-    newData = {
-        'selectedSymptoms': symps,
-        'gender': data.gender,
-        'yearOfBirth': data.yearOfBirth
-    }
+    path = []
+    alreadySeen = []
+    for s in symps:
+        if s not in alreadySeen:
+            path.append(f'selectedSymptoms={s}')
+            alreadySeen.append(s)
 
-    response = requests.post(API_URL, data=newData)
+
+    url = API_URL + '?' + '&'.join(path) + f'&gender={data.gender}&yearOfBirth={data.yearOfBirth}'
+    print(url)
+
+    response = requests.get(url)
 
     if response.status_code == 200:
-        return { 'result': response.text }
+        data = json.loads(response.text)
+        diags, specs = get_diagnostics_and_specializations(data)
+        return {
+            'diagnostics': (', ').join(diags),
+            'specialisation': (', ').join(specs)
+        }
     else:
         return { 'result': '' }
