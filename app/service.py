fed525e9444543fcc7b56a3e22953daefdaa1ae7
zeus <dhiogo.correa@itera.com.br>
2020-06-25 13:41:57 -0300

    added doctor

--- app/service.py
+++ app/service.py
@@ -38,7 +38,16 @@ def message(data):
         diags, specs = get_diagnostics_and_specializations(data)
         return {
             'diagnostics': (', ').join(diags),
-            'specialisation': (', ').join(specs)
+            'specialisation': (', ').join(specs),
+            'doctors': doctors(specs[0])
         }
     else:
         return { 'result': '' }
+
+
+def doctors(spec):
+    return {
+        'name': 'Dr. José Luiz',
+        'specialisation': spec,
+        'link_google': '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19526.16873180093!2d-46.69553786958027!3d-23.56755907037668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce577802a420bd%3A0x70a3d35a4dbd0699!2sAv.%20Rebou%C3%A7as%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1593103282773!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>' 
+    }
