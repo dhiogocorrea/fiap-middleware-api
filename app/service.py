f4fc2079f20524b0606ce8f49f0cef90dd95c18e
zeus <dhiogo.correa@itera.com.br>
2020-06-25 12:59:07 -0300

    api call

--- app/service.py
+++ app/service.py
@@ -1,7 +1,21 @@
 import requests
+import json
 
 API_URL = 'https://fiapdiagnosisapi.azurewebsites.net/Diagnostics'
 
 
 def message(data):
-    return { 'symps': data.selectedSymptoms }
+    symps = data.selectedSymptoms.split(',')
+
+    newData = {
+        'selectedSymptoms': symps,
+        'gender': data.gender,
+        'yearOfBirth': data.yearOfBirth
+    }
+
+    response = requests.post(API_URL, data=newData)
+
+    if response.status_code == 200:
+        return { 'result': response.text }
+    else:
+        returun { 'result': '' }
